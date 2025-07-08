dd4fac7db0f4976520ca80f0edb5413fff3843f4
hong-seung-kwan <hh8164@naver.com>
2025-07-08 17:41:41 +0900

    a

--- src/features/Home.js
+++ src/features/Home.js
@@ -31,6 +31,7 @@ const Home = () => {
             }
           }}
           locale={'ko'}
+          dayHeaderFormat={{ weekday: 'short' }}
           dayCellContent={({ date }) => {
             return date.getDate();
           }}
